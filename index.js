a19f2a9ec705d29f11f040c85eb1957f1edfdb09
ahamed-beep <muhamedahamed251@gmail.com>
2025-06-21 08:45:45 -0700

    Fix CORS config and update code

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['https://mu-project.netlify.app/', 'http://localhost:5173'],
+  origin: ['https://mu-project.netlify.app', 'http://localhost:5173'],
   credentials: true
 }));
+
+
+
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
