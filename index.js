0ec081d1bde26082da8a966295fbed8e47a6faf4
ahamed-beep <muhamedahamed251@gmail.com>
2025-07-07 08:40:31 -0700

    Updated submission guidelines and improved letter/photo layout design

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://new-upade.netlify.app'  // ✅ new domain
+    'https://new-things-added.netlify.app'  // ✅ new domain
   ],
   credentials: true
 }));
