013274bed98cadbe2a954e19066a65883617c5d8
ahamed-beep <muhamedahamed251@gmail.com>
2025-07-06 22:09:28 -0700

    your commit message here

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://new-heritage.netlify.app'  // ✅ new domain
+    'https://new-work-heritage.netlify.app'  // ✅ new domain
   ],
   credentials: true
 }));
